cf6512765f8c45c7a67d3edf49e32d7a7a7c0c09
ahadsheikh1814 <ahadsheikh1814@outlook.com>
2025-09-19 18:04:21 +0600

    add sanity cms

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,10 +1,11 @@
-/**
-* This configuration file lets you run `$ sanity [command]` in this folder
-* Go to https://www.sanity.io/docs/cli to learn more.
-**/
 import { defineCliConfig } from 'sanity/cli'
 
 const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
 const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET
 
-export default defineCliConfig({ api: { projectId, dataset } })
+export default defineCliConfig({
+  api: { projectId, dataset },
+  deployment: {
+    appId: 'riapn7jdef9vv2ltl9ghj6as', // the ID from your last deploy
+  },
+})
